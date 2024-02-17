9dab769663c9026fabae3d9b3bf5a26be6a93c63
KimGiheung <ewqdsacxzzaqxswcde@gmail.com>
2024-02-18 00:34:15 +0900

    2024-02-18

--- AI/drugfill산출물/drugfill산출물/소스코드모음/drugfill_predict_source_code/predict/PillName.py
+++
@@ -1,6 +0,0 @@
-class PillName:
-    def __init__(self, index, accuracy):
-        self.index = index
-        self.accuracy = accuracy
-    def __repr__(self):
-        return repr((self.index, self.accuracy))
