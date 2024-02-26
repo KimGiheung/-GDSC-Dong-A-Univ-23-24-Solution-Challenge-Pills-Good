e24c2a154f267fb77ff511be2557451bf3012578
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-02-26 13:36:27 +0900

    Ability to upload files to Firebase Storage and return the download URL of the uploaded file

--- frontend/pills_good_project/lib/storage_service.dart
+++ frontend/pills_good_project/lib/storage_service.dart
@@ -26,28 +26,3 @@ class Storage{
 
   }
 }
-
-
-/*
-import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
-import 'package:firebase_core/firebase_core.dart' as firebase_storage;
-
-class Storage{
-  final firebase_storage.FirebaseStorage=
-    firebase_storage.FilebaseStorage.instance;
-
-  Future<void> uploadFile(
-      String filePath,
-      String fileName,
-      )async{
-      File file=File(filePath);
-
-    try{
-      await storage.ref('test/$fileName').putFile(file);
-    }on firebase_core.FirebaseException catch (e) {
-      print(e);
-    }
-  }
-
-}
- */
